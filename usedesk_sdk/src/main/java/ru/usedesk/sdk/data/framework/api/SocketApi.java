e5957bdf4462d5e4458851b297b714a51c0a2d8d
Алексей Ельфимов <ya.chaosart@gmail.com>
2019-02-05 22:34:02 +0300

    EmitterListener перенесены в SocketApi

--- usedesk_sdk/src/main/java/ru/usedesk/sdk/data/framework/api/SocketApi.java
+++ usedesk_sdk/src/main/java/ru/usedesk/sdk/data/framework/api/SocketApi.java
@@ -5,19 +5,37 @@ import android.support.annotation.NonNull;
 import org.json.JSONException;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import ru.usedesk.sdk.R;
+import ru.usedesk.sdk.data.framework.ResponseProcessorImpl;
 import ru.usedesk.sdk.data.framework.entity.request.BaseRequest;
+import ru.usedesk.sdk.data.framework.entity.response.BaseResponse;
+import ru.usedesk.sdk.data.framework.entity.response.ErrorResponse;
+import ru.usedesk.sdk.data.framework.entity.response.InitChatResponse;
+import ru.usedesk.sdk.data.framework.entity.response.NewMessageResponse;
+import ru.usedesk.sdk.data.framework.entity.response.SendFeedbackResponse;
+import ru.usedesk.sdk.data.framework.entity.response.SetEmailResponse;
 import ru.usedesk.sdk.domain.entity.Constants;
+import ru.usedesk.sdk.domain.entity.UsedeskActionListener;
 import ru.usedesk.sdk.domain.entity.exceptions.ApiException;
 
 import static ru.usedesk.sdk.utils.LogUtils.LOGD;
+import static ru.usedesk.sdk.utils.LogUtils.LOGE;
 
 public class SocketApi {
     private static final String TAG = SocketApi.class.getSimpleName();
 
     private Socket socket;
+
+    private List<EmitterListener> emitterListeners = new ArrayList<>(4);
+
+    public SocketApi() {
+    }
 
     public boolean isConnected() {
         return socket.connected();
@@ -31,26 +49,29 @@ public class SocketApi {
         }
     }
 
-    public void connect() {
+    public void connect(UsedeskActionListener actionListener) {
         if (socket == null) {
             return;
         }
 
-        socket.on(Socket.EVENT_CONNECT, connectEmitterListener);
-        socket.on(Socket.EVENT_DISCONNECT, disconnectEmitterListener);
-        socket.on(Socket.EVENT_CONNECT_ERROR, connectErrorEmitterListener);
-        socket.on(Socket.EVENT_CONNECT_TIMEOUT, connectErrorEmitterListener);
-        socket.on(Constants.EVENT_SERVER_ACTION, baseEventEmitterListener);
+        emitterListeners.add(new BaseEventEmitterListener(actionListener));
+        emitterListeners.add(new ConnectEmitterListener(actionListener));
+        emitterListeners.add(new ConnectErrorEmitterListener(actionListener));
+        emitterListeners.add(new DisconnectEmitterListener(actionListener));
+
+        for (EmitterListener emitterListener : emitterListeners) {
+            emitterListener.attachSocket(socket);
+        }
 
         socket.connect();
     }
 
     public void disconnect() {
-        socket.off(Socket.EVENT_CONNECT, connectEmitterListener);
-        socket.off(Socket.EVENT_DISCONNECT, disconnectEmitterListener);
-        socket.off(Socket.EVENT_CONNECT_ERROR, connectErrorEmitterListener);
-        socket.off(Socket.EVENT_CONNECT_TIMEOUT, connectErrorEmitterListener);
-        socket.off(Constants.EVENT_SERVER_ACTION, baseEventEmitterListener);
+        for (EmitterListener emitterListener : emitterListeners) {
+            emitterListener.detachSocket(socket);
+        }
+
+        emitterListeners.clear();
 
         socket.disconnect();
     }
@@ -66,6 +87,85 @@ public class SocketApi {
             socket.emit(Constants.EVENT_SERVER_ACTION, baseRequest.toJSONObject());
         } catch (JSONException e) {
             throw new ApiException(e.getMessage());
+        }
+    }
+
+
+    private class ConnectEmitterListener extends EmitterListener {
+
+        public ConnectEmitterListener(UsedeskActionListener actionListener) {
+            super(actionListener, Socket.EVENT_CONNECT);
+        }
+
+        @Override
+        public void call(Object... args) {
+            LOGD(TAG, "ConnectEmitterListener.args = " + Arrays.toString(args));
+            initChat();
+        }
+    }
+
+    private class DisconnectEmitterListener extends EmitterListener {
+
+        public DisconnectEmitterListener(UsedeskActionListener actionListener) {
+            super(actionListener, Socket.EVENT_DISCONNECT);
+        }
+
+        @Override
+        public void call(Object... args) {
+            LOGE(TAG, "Disconnected.");
+
+            getActionListener().onDisconnected();
+        }
+    }
+
+    private class ConnectErrorEmitterListener extends EmitterListener {
+
+        public ConnectErrorEmitterListener(UsedeskActionListener actionListener) {
+            super(actionListener, Socket.EVENT_CONNECT_ERROR, Socket.EVENT_CONNECT_TIMEOUT);
+        }
+
+        @Override
+        public void call(Object... args) {
+            LOGE(TAG, "Error connecting: + " + Arrays.toString(args));
+
+            getActionListener().onError(R.string.message_connecting_error);
+        }
+    }
+
+    private class BaseEventEmitterListener extends EmitterListener {
+
+        private ResponseProcessorImpl responseProcessor = new ResponseProcessorImpl();
+
+        public BaseEventEmitterListener(UsedeskActionListener actionListener) {
+            super(actionListener, Constants.EVENT_SERVER_ACTION);
+        }
+
+        @Override
+        public void call(Object... args) {
+            String rawResponse = args[0].toString();
+
+            LOGD(TAG, "BaseEventEmitterListener.rawResponse = " + rawResponse);
+
+            BaseResponse response = responseProcessor.process(rawResponse);
+
+            if (response != null) {
+                switch (response.getType()) {
+                    case ErrorResponse.TYPE:
+                        parseErrorResponse((ErrorResponse) response);
+                        break;
+                    case InitChatResponse.TYPE:
+                        parseInitResponse((InitChatResponse) response);
+                        break;
+                    case SetEmailResponse.TYPE:
+                        break;
+                    case NewMessageResponse.TYPE:
+                        parseNewMessageResponse((NewMessageResponse) response);
+                        break;
+                    case SendFeedbackResponse.TYPE:
+                        parseFeedbackResponse((SendFeedbackResponse) response);
+                        break;
+                }
+            }
         }
     }
 }
--- usedesk_sdk/src/main/java/ru/usedesk/sdk/data/repository/ApiRepository.java
+++ usedesk_sdk/src/main/java/ru/usedesk/sdk/data/repository/ApiRepository.java
@@ -2,26 +2,12 @@ package ru.usedesk.sdk.data.repository;
 
 import android.support.annotation.NonNull;
 
-import java.util.Arrays;
-
-import io.socket.client.Socket;
-import ru.usedesk.sdk.R;
-import ru.usedesk.sdk.data.framework.ResponseProcessorImpl;
-import ru.usedesk.sdk.data.framework.api.EmitterListener;
 import ru.usedesk.sdk.data.framework.api.HttpApi;
 import ru.usedesk.sdk.data.framework.api.SocketApi;
 import ru.usedesk.sdk.data.framework.entity.request.BaseRequest;
-import ru.usedesk.sdk.data.framework.entity.response.BaseResponse;
-import ru.usedesk.sdk.data.framework.entity.response.ErrorResponse;
-import ru.usedesk.sdk.data.framework.entity.response.InitChatResponse;
-import ru.usedesk.sdk.data.framework.entity.response.NewMessageResponse;
-import ru.usedesk.sdk.data.framework.entity.response.SendFeedbackResponse;
-import ru.usedesk.sdk.data.framework.entity.response.SetEmailResponse;
-import ru.usedesk.sdk.domain.entity.Constants;
 import ru.usedesk.sdk.domain.entity.UsedeskActionListener;
 import ru.usedesk.sdk.domain.entity.exceptions.ApiException;
 
-import static ru.usedesk.sdk.utils.LogUtils.LOGD;
 import static ru.usedesk.sdk.utils.LogUtils.LOGE;
 
 public class ApiRepository {
@@ -32,11 +18,6 @@ public class ApiRepository {
 
     private UsedeskActionListener actionListener;
 
-    private BaseEventEmitterListener baseEventEmitterListener;
-    private ConnectEmitterListener connectEmitterListener;
-    private ConnectErrorEmitterListener connectErrorEmitterListener;
-    private DisconnectEmitterListener disconnectEmitterListener;
-
     public ApiRepository(@NonNull SocketApi socketApi, @NonNull HttpApi httpApi) {
         this.socketApi = socketApi;
         this.httpApi = httpApi;
@@ -44,11 +25,6 @@ public class ApiRepository {
 
     public void setActionListener(@NonNull UsedeskActionListener actionListener) {
         this.actionListener = actionListener;
-
-        baseEventEmitterListener = new BaseEventEmitterListener(actionListener);
-        connectEmitterListener = new ConnectEmitterListener(actionListener);
-        connectErrorEmitterListener = new ConnectErrorEmitterListener(actionListener);
-        disconnectEmitterListener = new DisconnectEmitterListener(actionListener);
     }
 
     public void emitterAction(BaseRequest baseRequest) {
@@ -78,85 +54,6 @@ public class ApiRepository {
     }
 
     public void connect() {
-        socketApi.connect();
-    }
-
-
-    private class ConnectEmitterListener extends EmitterListener {
-
-        public ConnectEmitterListener(UsedeskActionListener actionListener) {
-            super(actionListener, Socket.EVENT_CONNECT);
-        }
-
-        @Override
-        public void call(Object... args) {
-            LOGD(TAG, "ConnectEmitterListener.args = " + Arrays.toString(args));
-            initChat();
-        }
-    }
-
-    private class DisconnectEmitterListener extends EmitterListener {
-
-        public DisconnectEmitterListener(UsedeskActionListener actionListener) {
-            super(actionListener, Socket.EVENT_DISCONNECT);
-        }
-
-        @Override
-        public void call(Object... args) {
-            LOGE(TAG, "Disconnected.");
-
-            getActionListener().onDisconnected();
-        }
-    }
-
-    private class ConnectErrorEmitterListener extends EmitterListener {
-
-        public ConnectErrorEmitterListener(UsedeskActionListener actionListener) {
-            super(actionListener, Socket.EVENT_CONNECT_ERROR, Socket.EVENT_CONNECT_TIMEOUT);
-        }
-
-        @Override
-        public void call(Object... args) {
-            LOGE(TAG, "Error connecting: + " + Arrays.toString(args));
-
-            getActionListener().onError(R.string.message_connecting_error);
-        }
-    }
-
-    private class BaseEventEmitterListener extends EmitterListener {
-
-        private ResponseProcessorImpl responseProcessor = new ResponseProcessorImpl();
-
-        public BaseEventEmitterListener(UsedeskActionListener actionListener) {
-            super(actionListener, Constants.EVENT_SERVER_ACTION);
-        }
-
-        @Override
-        public void call(Object... args) {
-            String rawResponse = args[0].toString();
-
-            LOGD(TAG, "BaseEventEmitterListener.rawResponse = " + rawResponse);
-
-            BaseResponse response = responseProcessor.process(rawResponse);
-
-            if (response != null) {
-                switch (response.getType()) {
-                    case ErrorResponse.TYPE:
-                        parseErrorResponse((ErrorResponse) response);
-                        break;
-                    case InitChatResponse.TYPE:
-                        parseInitResponse((InitChatResponse) response);
-                        break;
-                    case SetEmailResponse.TYPE:
-                        break;
-                    case NewMessageResponse.TYPE:
-                        parseNewMessageResponse((NewMessageResponse) response);
-                        break;
-                    case SendFeedbackResponse.TYPE:
-                        parseFeedbackResponse((SendFeedbackResponse) response);
-                        break;
-                }
-            }
-        }
+        socketApi.connect(actionListener);
     }
 }

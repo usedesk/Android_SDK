c1065dbb5ea13bbd3a73badf25872e1d33111e00
Алексей Ельфимов <ya.chaosart@gmail.com>
2019-12-20 16:35:20 +0300

    Исправлена отправка имейла

--- usedesk_sdk/src/main/java/ru/usedesk/sdk/internal/data/framework/api/standard/entity/request/SetEmailRequest.java
+++ usedesk_sdk/src/main/java/ru/usedesk/sdk/internal/data/framework/api/standard/entity/request/SetEmailRequest.java
@@ -1,25 +1,30 @@
 package ru.usedesk.sdk.internal.data.framework.api.standard.entity.request;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 public class SetEmailRequest extends BaseRequest {
 
     public static final String TYPE = "@@server/chat/SET_EMAIL";
 
-    private String email;
-    private Payload payload;
+    private final Payload payload;
+    private final String email;
 
-    public SetEmailRequest(String token, String email, String name, Long phone, Long additionalId) {
+    public SetEmailRequest(@NonNull String token, @NonNull String email, @Nullable String name,
+                           @Nullable Long phone, @Nullable Long additionalId) {
         super(TYPE, token);
         this.email = email;
-        this.payload = new Payload(name, phone, additionalId);
+        this.payload = new Payload(email, name, phone, additionalId);
     }
 
     class Payload {
-        private String name;
-        private Long phone;
-        private Long additionalId;
-        private String email = "vatafak";
+        private final String email;
+        private final String name;
+        private final Long phone;
+        private final Long additionalId;
 
-        Payload(String name, Long phone, Long additionalId) {
+        Payload(@NonNull String email, @Nullable String name, @Nullable Long phone, @Nullable Long additionalId) {
+            this.email = email;
             this.name = name;
             this.phone = phone;
             this.additionalId = additionalId;

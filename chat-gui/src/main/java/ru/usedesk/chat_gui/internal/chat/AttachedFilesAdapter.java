82753a62b929893d45cbb5b766946f70333a81cb
Алексей Ельфимов <ya.chaosart@gmail.com>
2020-01-20 16:39:49 +0300

    Добавлено превью видео прикреплённых файлов

--- chat-gui/src/main/java/ru/usedesk/chat_gui/internal/chat/AttachedFilesAdapter.java
+++ chat-gui/src/main/java/ru/usedesk/chat_gui/internal/chat/AttachedFilesAdapter.java
@@ -65,17 +65,11 @@ public class AttachedFilesAdapter extends RecyclerView.Adapter<AttachedFilesAdap
         }
 
         private void bind(@NonNull UsedeskFileInfo usedeskFileInfo) {
-            if (usedeskFileInfo.getType().equals(UsedeskFileInfo.Type.IMAGE)) {
-                GlideApp.with(imageViewPreview)
-                        .load(usedeskFileInfo.getUri())
-                        .centerCrop()
-                        .error(R.drawable.ic_attachment_photo_black)
-                        .into(imageViewPreview);
-            } else {
-                GlideApp.with(imageViewPreview)
-                        .load(R.drawable.ic_document_black)
-                        .into(imageViewPreview);
-            }
+            GlideApp.with(imageViewPreview)
+                    .load(usedeskFileInfo.getUri())
+                    .centerCrop()
+                    .error(R.drawable.ic_document_black)
+                    .into(imageViewPreview);
 
             imageViewDetach.setOnClickListener(v -> chatViewModel.detachFile(usedeskFileInfo));
         }

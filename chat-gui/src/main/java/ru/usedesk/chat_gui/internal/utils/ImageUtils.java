efaddd6cff4ae37b2efeb932f261ffe749520f92
Алексей Ельфимов <ya.chaosart@gmail.com>
2020-03-25 11:34:31 +0300

    Убрано использование AppGlideModule

--- chat-gui/src/main/java/ru/usedesk/chat_gui/internal/utils/ImageUtils.java
+++ chat-gui/src/main/java/ru/usedesk/chat_gui/internal/utils/ImageUtils.java
@@ -1,6 +1,7 @@
 package ru.usedesk.chat_gui.internal.utils;
 
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
@@ -23,18 +25,28 @@ public class ImageUtils {
                                             @NonNull String pictureUrl, int errorResId) {
         imageImageView.setImageResource(errorResId);
         if (!TextUtils.isEmpty(pictureUrl)) {
-            GlideApp.with(imageImageView)
+            Glide.with(imageImageView)
                     .load(pictureUrl)
                     .error(errorResId)
                     .into(imageImageView);
         }
     }
 
+    public static void setImage(@NonNull ImageView target,
+                                @NonNull Uri uri,
+                                int errorId) {
+        Glide.with(target)
+                .load(uri)
+                .centerCrop()
+                .error(errorId)
+                .into(target);
+    }
+
     public static void checkForDisplayImage(@NonNull ImageView imageImageView,
                                             @NonNull ProgressBar progressBar,
                                             @NonNull String pictureUrl) {
         if (!TextUtils.isEmpty(pictureUrl)) {
-            GlideApp.with(imageImageView)
+            Glide.with(imageImageView)
                     .load(pictureUrl)
                     .listener(new RequestListener<Drawable>() {
                         @Override

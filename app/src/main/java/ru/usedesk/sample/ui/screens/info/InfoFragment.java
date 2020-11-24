9269a7928d221400bc11df5e37b44667a0552271
Алексей Ельфимов <ya.chaosart@gmail.com>
2020-11-24 15:43:10 +0300

    В окне инфомрации версия обновляется самостоятельно, ссылки кликабельные

--- app/src/main/java/ru/usedesk/sample/ui/screens/info/InfoFragment.java
+++ app/src/main/java/ru/usedesk/sample/ui/screens/info/InfoFragment.java
@@ -5,9 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import ru.usedesk.sample.BuildConfig;
 import ru.usedesk.sample.R;
+import ru.usedesk.sample.databinding.FragmentInfoBinding;
 
 public class InfoFragment extends Fragment {
 
@@ -16,7 +20,13 @@ public class InfoFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentInfoBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_info,
+                container, false);
+
+        binding.tvSdkVer.setText("SDK v" + BuildConfig.VERSION_NAME);
+
+        return binding.getRoot();
     }
 }

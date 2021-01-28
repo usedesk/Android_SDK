ee5e07b1a67a3de078e6d7d71d19f5182cf57dfd
Алексей Ельфимов <ya.chaosart@gmail.com>
2021-01-28 20:15:11 +0300

    Исправлено сохранение конфигурации, добавлены версии и миграция

--- chat-sdk/src/main/java/ru/usedesk/chat_sdk/data/repository/configuration/loader/configuration/ConfigurationLoader.kt
+++ chat-sdk/src/main/java/ru/usedesk/chat_sdk/data/repository/configuration/loader/configuration/ConfigurationLoader.kt
@@ -2,6 +2,7 @@ package ru.usedesk.chat_sdk.data.repository.configuration.loader.configuration
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.google.gson.Gson
 import ru.usedesk.chat_sdk.data.repository._extra.DataLoader
 import ru.usedesk.chat_sdk.entity.UsedeskChatConfiguration
 import toothpick.InjectConstructor
@@ -16,79 +17,101 @@ internal class ConfigurationLoader(
             Context.MODE_MULTI_PROCESS
     )
 
-    private fun getString(value: Long?): String? {
-        return value?.toString()
-    }
+    private val gson: Gson = Gson()
 
     override fun loadData(): UsedeskChatConfiguration? {
-        val urlChat = sharedPreferences.getString(KEY_URL_CHAT, null)
-        val urlOfflineForm = sharedPreferences.getString(KEY_URL_OFFLINE_FORM, null)
-        val urlToSendFile = sharedPreferences.getString(KEY_URL_TO_SEND_FILE, null)
-        val clientId = sharedPreferences.getString(KEY_ID, null)
-        val clientEmail = sharedPreferences.getString(KEY_EMAIL, null)
-        val clientInitMessage = sharedPreferences.getString(KEY_CLIENT_INIT_MESSAGE, null)
-        if (urlChat == null
-                || urlOfflineForm == null
-                || urlToSendFile == null
-                || clientId == null
-                || clientEmail == null) {
-            return null
+        val version = sharedPreferences.getInt(KEY_VERSION, 1)
+        if (version < CURRENT_VERSION) {
+            migrate(version)
         }
-        var clientName: String? = null
-        var clientPhone: String? = null
-        var clientAdditionalId: String? = null
-        try {
-            clientName = sharedPreferences.getString(KEY_NAME, null)
-            clientPhone = sharedPreferences.getString(KEY_PHONE, null)
-            clientAdditionalId = sharedPreferences.getString(KEY_ADDITIONAL_ID, null)
-        } catch (e: ClassCastException) {
-            try {
-                clientPhone = sharedPreferences.getLong(KEY_PHONE, 0).toString() //Для миграции с версий, где хранился Long
-                clientAdditionalId = sharedPreferences.getLong(KEY_ADDITIONAL_ID, 0).toString()
-            } catch (e1: ClassCastException) {
-                e.printStackTrace()
-            }
+
+        return try {
+            val json = sharedPreferences.getString(KEY_DATA, null)
+            gson.fromJson(json, UsedeskChatConfiguration::class.java)
+        } catch (e: Exception) {
+            null
         }
-        return UsedeskChatConfiguration(
-                urlChat,
-                urlOfflineForm,
-                urlToSendFile,
-                clientId,
-                clientEmail,
-                clientName,
-                clientPhone?.toLongOrNull(),
-                clientAdditionalId?.toLongOrNull(),
-                clientInitMessage)
     }
 
-    override fun saveData(configuration: UsedeskChatConfiguration) {
+    private fun migrate(oldVersion: Int) {
+        val configuration = loadLegacy(oldVersion)
+        if (configuration != null) {
+            saveData(configuration)
+        } else {
+            clearData()
+        }
+    }
+
+    override fun saveData(data: UsedeskChatConfiguration) {
+        val json = gson.toJson(data)
+
         sharedPreferences.edit()
-                .putString(KEY_ID, configuration.companyId)
-                .putString(KEY_URL_CHAT, configuration.urlChat)
-                .putString(KEY_URL_OFFLINE_FORM, configuration.urlOfflineForm)
-                .putString(KEY_EMAIL, configuration.email)
-                .putString(KEY_NAME, configuration.clientName)
-                .putString(KEY_ADDITIONAL_ID, getString(configuration.clientAdditionalId))
-                .putString(KEY_CLIENT_INIT_MESSAGE, configuration.clientInitMessage)
-                .putString(KEY_PHONE, getString(configuration.clientPhoneNumber))
+                .putString(KEY_DATA, json)
+                .putInt(KEY_VERSION, CURRENT_VERSION)
                 .apply()
     }
 
     override fun clearData() {
         sharedPreferences.edit()
-                .remove(KEY_ID)
-                .remove(KEY_URL_CHAT)
-                .remove(KEY_URL_OFFLINE_FORM)
-                .remove(KEY_EMAIL)
+                .putInt(KEY_VERSION, CURRENT_VERSION)
+                .remove(KEY_DATA)
                 .apply()
     }
 
+    private fun loadLegacy(oldVersion: Int): UsedeskChatConfiguration? {
+        if (oldVersion == 1) {
+            val urlChat = sharedPreferences.getString(KEY_URL_CHAT, null)
+            val urlOfflineForm = sharedPreferences.getString(KEY_URL_OFFLINE_FORM, null)
+            val clientId = sharedPreferences.getString(KEY_ID, null)
+            val clientEmail = sharedPreferences.getString(KEY_EMAIL, null)
+            val clientInitMessage = sharedPreferences.getString(KEY_CLIENT_INIT_MESSAGE, null)
+            if (urlChat != null
+                    && urlOfflineForm != null
+                    && clientId != null
+                    && clientEmail != null) {
+                var clientName: String? = null
+                var clientPhone: String? = null
+                var clientAdditionalId: String? = null
+                try {
+                    clientName = sharedPreferences.getString(KEY_NAME, null)
+                    clientPhone = sharedPreferences.getString(KEY_PHONE, null)
+                    clientAdditionalId = sharedPreferences.getString(KEY_ADDITIONAL_ID, null)
+                } catch (e: ClassCastException) {
+                    try {
+                        clientPhone = sharedPreferences.getLong(KEY_PHONE, 0).toString() //Для миграции с версий, где хранился Long
+                        clientAdditionalId = sharedPreferences.getLong(KEY_ADDITIONAL_ID, 0).toString()
+                    } catch (e1: ClassCastException) {
+                        e.printStackTrace()
+                    }
+                }
+                return UsedeskChatConfiguration(
+                        urlChat,
+                        urlOfflineForm,
+                        "https://secure.usedesk.ru/uapi/v1/send_file",
+                        clientId,
+                        clientEmail,
+                        clientName,
+                        clientPhone?.toLongOrNull(),
+                        clientAdditionalId?.toLongOrNull(),
+                        clientInitMessage)
+            }
+        }
+
+        return null
+    }
+
     companion object {
+        private const val CURRENT_VERSION = 2
+
         private const val PREF_NAME = "usedeskSdkConfiguration"
+
+        private const val KEY_VERSION = "versionKey"
+        private const val KEY_DATA = "dataKey"
+
+        //Legacy:
         private const val KEY_ID = "id"
         private const val KEY_URL_CHAT = "url"
         private const val KEY_URL_OFFLINE_FORM = "offlineUrl"
-        private const val KEY_URL_TO_SEND_FILE = "urlToSendFile"
         private const val KEY_EMAIL = "email"
         private const val KEY_NAME = "name"
         private const val KEY_PHONE = "phone"

f9787cd72901b8b98a9223b495e68eb561a69b39
Алексей Ельфимов <ya.chaosart@gmail.com>
2021-02-09 21:50:04 +0300

    Исправления OkHttpClient для работы на старых версиях

---
+++ common-sdk/src/main/java/ru/usedesk/common_sdk/internal/api/UsedeskOkHttpClientFactory.java
@@ -0,0 +1,142 @@
+package ru.usedesk.common_sdk.internal.api;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.security.ProviderInstaller;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.inject.Inject;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
+
+public class UsedeskOkHttpClientFactory {
+
+    private final Context appContext;
+
+    @Inject
+    public UsedeskOkHttpClientFactory(@NonNull Context appContext) {
+        this.appContext = appContext;
+    }
+
+    @NonNull
+    public OkHttpClient createInstance() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            try {
+                ProviderInstaller.installIfNeeded(appContext);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                applyProtocol(builder, TlsVersion.TLS_1_2.javaName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return builder.build();
+    }
+
+    private void applyProtocol(@NonNull OkHttpClient.Builder builder,
+                               @NonNull String protocolName) throws NoSuchAlgorithmException, KeyManagementException {
+        X509TrustManager allTrustManager = new X509TrustManager() {
+            @SuppressLint("TrustAllX509TrustManager")
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) {
+
+            }
+
+            @SuppressLint("TrustAllX509TrustManager")
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance(protocolName);
+        sslContext.init(null, new X509TrustManager[]{allTrustManager}, new SecureRandom());
+
+        SSLSocketFactory sslSocketFactory = new ProtocolSocketFactory(sslContext.getSocketFactory(), protocolName);
+
+        builder.sslSocketFactory(sslSocketFactory, allTrustManager);
+        builder.hostnameVerifier((hostname, session) -> true);
+    }
+
+    private class ProtocolSocketFactory extends SSLSocketFactory {
+        private final SSLSocketFactory sslSocketFactory;
+        private final String[] protocols;
+
+        public ProtocolSocketFactory(@NonNull SSLSocketFactory sslSocketFactory,
+                                     @NonNull String protocol) {
+            this.sslSocketFactory = sslSocketFactory;
+            this.protocols = new String[]{protocol};
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return sslSocketFactory.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return sslSocketFactory.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+            return enableProtocolOnSocket(sslSocketFactory.createSocket(s, host, port, autoClose));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port) throws IOException {
+            return enableProtocolOnSocket(sslSocketFactory.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException {
+            return enableProtocolOnSocket(sslSocketFactory.createSocket(host, port, localHost, localPort));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress host, int port) throws IOException {
+            return enableProtocolOnSocket(sslSocketFactory.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+            return enableProtocolOnSocket(sslSocketFactory.createSocket(address, port, localAddress, localPort));
+        }
+
+        private Socket enableProtocolOnSocket(@Nullable Socket socket) {
+            if (socket instanceof SSLSocket) {
+                ((SSLSocket) socket).setEnabledProtocols(protocols);
+            }
+            return socket;
+        }
+    }
+}
--- knowledgebase-sdk/src/main/java/ru/usedesk/knowledgebase_sdk/internal/di/MainModule.java
+++ knowledgebase-sdk/src/main/java/ru/usedesk/knowledgebase_sdk/internal/di/MainModule.java
@@ -6,20 +6,17 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 
-import javax.inject.Named;
-
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+import ru.usedesk.common_sdk.internal.api.IUsedeskApiFactory;
+import ru.usedesk.common_sdk.internal.api.UsedeskApiFactory;
+import ru.usedesk.common_sdk.internal.api.UsedeskOkHttpClientFactory;
 import ru.usedesk.knowledgebase_sdk.external.IUsedeskKnowledgeBase;
 import ru.usedesk.knowledgebase_sdk.external.entity.UsedeskKnowledgeBaseConfiguration;
 import ru.usedesk.knowledgebase_sdk.internal.data.framework.retrofit.ApiLoader;
 import ru.usedesk.knowledgebase_sdk.internal.data.framework.retrofit.ApiRetrofit;
 import ru.usedesk.knowledgebase_sdk.internal.data.framework.retrofit.IApiLoader;
-import ru.usedesk.knowledgebase_sdk.internal.data.framework.retrofit.entity.RetrofitEnumConverterFactory;
 import ru.usedesk.knowledgebase_sdk.internal.data.repository.ApiRepository;
 import ru.usedesk.knowledgebase_sdk.internal.data.repository.IKnowledgeBaseRepository;
 import ru.usedesk.knowledgebase_sdk.internal.domain.KnowledgeBaseInteractor;
@@ -27,7 +24,8 @@ import toothpick.config.Module;
 
 class MainModule extends Module {
 
-    MainModule(@NonNull Context appContext, @NonNull UsedeskKnowledgeBaseConfiguration knowledgeBaseConfiguration) {
+    MainModule(@NonNull Context appContext,
+               @NonNull UsedeskKnowledgeBaseConfiguration knowledgeBaseConfiguration) {
         bind(Context.class).toInstance(appContext);
         bind(UsedeskKnowledgeBaseConfiguration.class).toInstance(knowledgeBaseConfiguration);
 
@@ -38,43 +36,16 @@ class MainModule extends Module {
 
         bind(IApiLoader.class).to(ApiLoader.class);
 
-        bind(ApiRetrofit.class).toInstance(apiRetrofit(retrofit()));
+        bind(ApiRetrofit.class).toInstance(apiRetrofit(appContext));
 
         bind(Scheduler.class).withName("work").toInstance(Schedulers.io());
         bind(Scheduler.class).withName("main").toInstance(AndroidSchedulers.mainThread());
     }
 
-    @NonNull
-    @Named("serverBaseUrl")
-    private String serverBaseUrl() {
-        return "https://api.usedesk.ru/support/";
-    }
-
-    @NonNull
-    private OkHttpClient okHttpClient() {
-        return new OkHttpClient.Builder().build();
-    }
-
-
-    private Retrofit retrofit() {
-        return new Retrofit.Builder()
-                .baseUrl(serverBaseUrl())
-                .client(okHttpClient())
-                .addConverterFactory(scalarsConverterFactory())
-                .addConverterFactory(retrofitEnumConverterFactory())
-                .build();
-    }
-
-    private RetrofitEnumConverterFactory retrofitEnumConverterFactory() {
-        return new RetrofitEnumConverterFactory();
-    }
-
-    private ScalarsConverterFactory scalarsConverterFactory() {
-        return ScalarsConverterFactory.create();
-    }
-
-    private ApiRetrofit apiRetrofit(@NonNull final Retrofit retrofit) {
-        return retrofit.create(ApiRetrofit.class);
+    private ApiRetrofit apiRetrofit(@NonNull Context appContext) {
+        UsedeskOkHttpClientFactory okHttpClientFactory = new UsedeskOkHttpClientFactory(appContext);
+        IUsedeskApiFactory apiFactory = new UsedeskApiFactory(gson(), okHttpClientFactory);
+        return apiFactory.getInstance("https://api.usedesk.ru/support/", ApiRetrofit.class);
     }
 
     private Gson gson() {

bd9c8a12c1b79b187d8daacf5a5df128e7ecf9ee
Ельфимов Алексей <elfimov.alexey@mail.ru>
2022-01-20 18:20:47 +0300

    Добавлено отображение ошибки загрузки чата

--- chat-gui/src/main/java/ru/usedesk/chat_gui/chat/loading/LoadingPage.kt
+++ chat-gui/src/main/java/ru/usedesk/chat_gui/chat/loading/LoadingPage.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.viewModels
 import ru.usedesk.chat_gui.R
 import ru.usedesk.common_gui.UsedeskBinding
 import ru.usedesk.common_gui.UsedeskFragment
 import ru.usedesk.common_gui.inflateItem
 
 internal class LoadingPage : UsedeskFragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val binding = inflateItem(
+
+    private val viewModel: LoadingViewModel by viewModels()
+
+    private lateinit var binding: Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = inflateItem(
             inflater,
             container,
             R.layout.usedesk_page_loading,
@@ -21,7 +32,67 @@ internal class LoadingPage : UsedeskFragment() {
             Binding(rootView, defaultStyleId)
         }
 
+        binding.styleValues.getStyle(R.attr.usedesk_chat_screen_loading_image)
+
         return binding.rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.modelLiveData.initAndObserveWithOld(viewLifecycleOwner) { old, new ->
+            if (old?.state != new.state) {
+                when (new.state) {
+                    LoadingViewModel.State.LOADING -> {
+                        binding.imageBinding.ivLoadingImage.setImageResourceSafe(
+                            binding.imageBinding.loadingImageId
+                        )
+                        binding.titleBinding.tvLoadingTitle.setTextSafe(
+                            binding.titleBinding.loadingTitleId
+                        )
+                        binding.textBinding.tvLoadingText.setTextSafe(
+                            binding.textBinding.loadingTextId
+                        )
+                    }
+                    LoadingViewModel.State.NO_INTERNET -> {
+                        binding.imageBinding.ivLoadingImage.setImageResourceSafe(
+                            binding.imageBinding.noInternetImageId
+                        )
+                        binding.titleBinding.tvLoadingTitle.setTextSafe(
+                            binding.titleBinding.noInternetTitleId
+                        )
+                        binding.textBinding.tvLoadingText.setTextSafe(
+                            binding.textBinding.noInternetTextId
+                        )
+                    }
+                    else -> {
+                        binding.imageBinding.ivLoadingImage.setImageResourceSafe(0)
+                        binding.titleBinding.tvLoadingTitle.setTextSafe(0)
+                        binding.textBinding.tvLoadingText.setTextSafe(0)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun ImageView.setImageResourceSafe(
+        resourceId: Int
+    ) {
+        if (resourceId == 0) {
+            setImageDrawable(null)
+            visibility = View.GONE
+        } else {
+            setImageResource(resourceId)
+            visibility = View.VISIBLE
+        }
+    }
+
+    private fun TextView.setTextSafe(resourceId: Int) {
+        if (resourceId == 0) {
+            text = ""
+        } else {
+            setText(resourceId)
+        }
     }
 
     companion object {
@@ -30,7 +101,40 @@ internal class LoadingPage : UsedeskFragment() {
         }
     }
 
-    internal class Binding(rootView: View, defaultStyleId: Int) : UsedeskBinding(rootView, defaultStyleId) {
-        val tvLoading: TextView = rootView.findViewById(R.id.tv_loading)
+    internal class Binding(rootView: View, defaultStyleId: Int) :
+        UsedeskBinding(rootView, defaultStyleId) {
+        val imageBinding = ImageBinding(
+            rootView.findViewById(R.id.iv_loading_image),
+            styleValues.getStyle(R.attr.usedesk_chat_screen_loading_image)
+        )
+        val titleBinding = TitleBinding(
+            rootView.findViewById(R.id.tv_loading_title),
+            styleValues.getStyle(R.attr.usedesk_chat_screen_loading_title)
+        )
+        val textBinding = TextBinding(
+            rootView.findViewById(R.id.tv_loading_text),
+            styleValues.getStyle(R.attr.usedesk_chat_screen_loading_text)
+        )
+    }
+
+    internal class ImageBinding(rootView: View, defaultStyleId: Int) :
+        UsedeskBinding(rootView, defaultStyleId) {
+        val ivLoadingImage = rootView as ImageView
+        val loadingImageId = styleValues.getIdOrZero(R.attr.usedesk_drawable_1)
+        val noInternetImageId = styleValues.getIdOrZero(R.attr.usedesk_drawable_2)
+    }
+
+    internal class TitleBinding(rootView: View, defaultStyleId: Int) :
+        UsedeskBinding(rootView, defaultStyleId) {
+        val tvLoadingTitle = rootView as TextView
+        val loadingTitleId = styleValues.getIdOrZero(R.attr.usedesk_text_1)
+        val noInternetTitleId = styleValues.getIdOrZero(R.attr.usedesk_text_2)
+    }
+
+    internal class TextBinding(rootView: View, defaultStyleId: Int) :
+        UsedeskBinding(rootView, defaultStyleId) {
+        val tvLoadingText = rootView as TextView
+        val loadingTextId = styleValues.getIdOrZero(R.attr.usedesk_text_1)
+        val noInternetTextId = styleValues.getIdOrZero(R.attr.usedesk_text_2)
     }
 }

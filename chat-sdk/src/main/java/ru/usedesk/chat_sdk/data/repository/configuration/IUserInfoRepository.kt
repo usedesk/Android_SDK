71a2c66415370d26286bec1eca4994dd7b099b15
Elfimov Alexey <elfimov.alexey@mail.ru>
2022-12-12 16:41:27 +0300

    Исправлено раздвоение и сброс чата

--- chat-sdk/src/main/java/ru/usedesk/chat_sdk/data/repository/configuration/IUserInfoRepository.kt
+++ chat-sdk/src/main/java/ru/usedesk/chat_sdk/data/repository/configuration/IUserInfoRepository.kt
@@ -3,7 +3,7 @@ package ru.usedesk.chat_sdk.data.repository.configuration
 import ru.usedesk.chat_sdk.entity.UsedeskChatConfiguration
 
 internal interface IUserInfoRepository {
-    fun getConfiguration(configuration: UsedeskChatConfiguration): UsedeskChatConfiguration?
+    fun getConfiguration(): UsedeskChatConfiguration?
 
-    fun setConfiguration(configuration: UsedeskChatConfiguration)
+    fun updateConfiguration(onUpdate: UsedeskChatConfiguration.() -> UsedeskChatConfiguration)
 }

4ae7bd5194b8bb4351d38cecea47562c8b40f95c
Elfimov Alexey <elfimov.alexey@mail.ru>
2023-01-13 12:50:45 +0300

    Добавлен флаг FLAG_IMMUTABLE в PendingIntent сервиса уведомлений

--- app/src/main/java/ru/usedesk/sample/service/CustomSimpleNotificationsService.kt
+++ app/src/main/java/ru/usedesk/sample/service/CustomSimpleNotificationsService.kt
@@ -2,6 +2,7 @@ package ru.usedesk.sample.service
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.os.Build
 import ru.usedesk.chat_sdk.service.notifications.UsedeskNotificationsServiceFactory
 import ru.usedesk.chat_sdk.service.notifications.view.UsedeskSimpleNotificationsService
 import ru.usedesk.sample.ui.main.MainActivity
@@ -10,7 +11,15 @@ class CustomSimpleNotificationsService : UsedeskSimpleNotificationsService() {
     override fun getContentPendingIntent(): PendingIntent? {
         val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-        return PendingIntent.getActivity(this, 0, intent, 0)
+        return PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> PendingIntent.FLAG_IMMUTABLE
+                else -> 0
+            }
+        )
     }
 
     override fun getDeletePendingIntent(): PendingIntent? = null

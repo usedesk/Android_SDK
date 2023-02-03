62b7fd2b988a3e0c9910e96d4715e4fe5904ce05
Elfimov Alexey <elfimov.alexey@mail.ru>
2023-02-03 19:15:22 +0300

    Доработка загрузки превью

--- chat-sdk/src/main/java/ru/usedesk/chat_sdk/data/repository/thumbnail/ThumbnailRepository.kt
+++ chat-sdk/src/main/java/ru/usedesk/chat_sdk/data/repository/thumbnail/ThumbnailRepository.kt
@@ -11,15 +11,13 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import ru.usedesk.chat_sdk.entity.UsedeskMessage
 import ru.usedesk.chat_sdk.entity.UsedeskMessageOwner
-import ru.usedesk.common_sdk.UsedeskLog
 import java.io.File
 import javax.inject.Inject
 
 internal class ThumbnailRepository @Inject constructor(
-    appContext: Context
+    private val appContext: Context
 ) : IThumbnailRepository {
     private val cacheDir = appContext.cacheDir
-
     private val handledSet = mutableSetOf<Long>()
     private val ioScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     private val mutex = Mutex()
@@ -29,9 +27,9 @@ internal class ThumbnailRepository @Inject constructor(
     override fun loadThumbnail(message: UsedeskMessage.File) {
         if (message.file.isVideo()) {
             ioScope.launch {
+                val id = message.id
+                val localId = (message as? UsedeskMessageOwner.Client)?.localId ?: id
                 mutex.withLock {
-                    val id = message.id
-                    val localId = (message as? UsedeskMessageOwner.Client)?.localId ?: id
                     if (!handledSet.contains(localId) || !handledSet.contains(id)) {
                         handledSet.add(id)
                         launchLoadThumbnail(id, localId, message.file.content.toUri())
@@ -48,34 +46,51 @@ internal class ThumbnailRepository @Inject constructor(
         ioScope.launch {
             val thumbnailFile = localId.toFile()
             val thumbnailUri = if (thumbnailFile.exists()) {
-                UsedeskLog.onLog("exist") { thumbnailFile.toString() }
                 when (localId) {
                     id -> thumbnailFile.toUri()
                     else -> {
                         val newThumbnailFile = id.toFile()
-                        UsedeskLog.onLog("renamed") { "$thumbnailFile to $newThumbnailFile" }
                         thumbnailFile.renameTo(newThumbnailFile)
                         newThumbnailFile.toUri()
                     }
                 }
             } else {
+                val path = videoUri.toString()
                 val media = MediaMetadataRetriever()
-                try {
-                    val path = videoUri.toString()
-                    when (videoUri.scheme) {
-                        "file" -> media.setDataSource(path)
-                        else -> media.setDataSource(path, mapOf())
+                val thumbnailBitmap = try {
+                    try {
+                        media.setDataSource(path)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        null
+                    } ?: try {
+                        media.setDataSource(path, mapOf())
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        null
+                    } ?: try {
+                        media.setDataSource(appContext, videoUri)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
                     }
-                    when (val thumbnailBitmap = media.getFrameAtTime(0)) {
+                    //На разных устройствах срабатывают разные методы
+
+                    media.getFrameAtTime(0)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    null
+                } finally {
+                    media.release()
+                }
+                try {
+                    when (thumbnailBitmap) {
                         null -> null
-                        else -> {
-                            thumbnailFile.outputStream().use { out ->
-                                thumbnailBitmap.compress(
-                                    Bitmap.CompressFormat.JPEG,
-                                    100,
-                                    out
-                                )
-                            }
+                        else -> thumbnailFile.outputStream().use { out ->
+                            thumbnailBitmap.compress(
+                                Bitmap.CompressFormat.JPEG,
+                                100,
+                                out
+                            )
                             thumbnailFile.toUri()
                         }
                     }
@@ -83,7 +98,7 @@ internal class ThumbnailRepository @Inject constructor(
                     e.printStackTrace()
                     null
                 } finally {
-                    media.release()
+                    thumbnailBitmap?.recycle()
                 }
             }
             if (thumbnailUri == null) {
@@ -93,8 +108,6 @@ internal class ThumbnailRepository @Inject constructor(
                 when (thumbnailUri) {
                     null -> handledSet.remove(id)
                     else -> thumbnailMapFlow.value = thumbnailMapFlow.value.toMutableMap().apply {
-                        UsedeskLog.onLog(localId.toString()) { thumbnailUri.toString() }
-                        UsedeskLog.onLog(id.toString()) { thumbnailUri.toString() }
                         set(localId, thumbnailUri)
                         if (localId != id) {
                             set(id, thumbnailUri)

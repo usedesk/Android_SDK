3dfa2ce5ec86032227a13e3508c92453656a9dbe
Elfimov Alexey <elfimov.alexey@mail.ru>
2023-03-10 12:54:31 +0300

    minSdk upgraded to 21, targetSdk upgraded to 33, libraries versions upgraded

--- chat-gui/src/main/java/ru/usedesk/chat_gui/chat/UsedeskChatScreen.kt
+++ chat-gui/src/main/java/ru/usedesk/chat_gui/chat/UsedeskChatScreen.kt
@@ -106,7 +106,7 @@ class UsedeskChatScreen : UsedeskFragment() {
             requireContext(),
             chatConfiguration
         )
-        findParent<IUsedeskOnChatInitedListener>()?.onChatInited(usedeskChat)
+        findParent<IUsedeskOnChatInitedListener>()?.onChatInited(usedeskChat) //TODO: вызывается 1 раз?
 
         val toolbarAdapter = UsedeskToolbarAdapter(toolbar).apply {
             setBackButton(requireActivity()::onBackPressed)
